c5985b23d2e836a2ac95b151b2eac4affe905e94
River yang <river@ryc.one>
2020-02-09 01:02:38 +1100

    add statistic as report

--- pkg/ping/ping.go
+++ pkg/ping/ping.go
@@ -2,14 +2,38 @@ package ping
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"os"
+	"os/signal"
+	"runtime/pprof"
 	"time"
 )
 
-func TCPPing(hostname, port, IPv4Addr, network string, pingLimit int) {
+func exitReport(hostname, port, network string, total, received *int) {
+	// capture ctrl+c and stop CPU profiler
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			fmt.Printf("--- %s:%s %s ping statistics ---\n", hostname, port, network)
+			failureRate := (*total - *received) * 100 / *total
+			fmt.Printf("%d packets transmitted, %d packets received, %d %% packet loss\n", *total, *received, failureRate)
+			log.Printf("captured %v, stopping profiler and exiting..", sig)
+			pprof.StopCPUProfile()
+			os.Exit(1)
+		}
+	}()
+}
+
+func TCPPing(hostname, port, IPv4Addr, network string) {
 	fmt.Printf("PING %s:%s (%s):\n", hostname, port, IPv4Addr) //TODO: pick first IPV4 address
-	for i := 1; i < pingLimit; i++ {
+	r := 0
+	i := 0
+	exitReport(hostname, port, network, &i, &r)
+	for {
 		time.Sleep(1 * time.Second)
+		i = i + 1
 		now := time.Now()
 		connDial, err := net.DialTimeout(network, hostname+":"+port, 5*time.Second)
 		if err != nil {
@@ -22,5 +46,6 @@ func TCPPing(hostname, port, IPv4Addr, network string, pingLimit int) {
 		done := time.Now()
 		latency := done.Sub(now)
 		fmt.Printf("dial %s %s: from %s tcp_seq=%d time=%d ms\n", network, remoteAddrPort, localAddrPort, i, latency.Milliseconds())
+		r = r + 1
 	}
 }
